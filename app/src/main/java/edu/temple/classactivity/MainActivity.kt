df9c623cfc4b03a917ce0cd4f9c6e4c8a6d8a183
Stephanie <stephaniemillwood25@gmail.com>
2025-01-28 12:15:04 -0500

    Added onClickListener to button. (a few different versions)

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,37 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById<Button>(R.id.clickme)!!).setOnClickListener{
+            (it as Button).text = "Button clicked !!"
+        }
+
+//        findViewById<Button>(R.id.clickme)?.run{
+//            this.text = "Button clicked 4"
+//        }
+
+        //val ocl = OnClickListener { p0 -> (p0 as Button).text = "Button clicked 3" }
+
+    //       clickMeButton = findViewById(R.id.clickme)
+    //       clickMeButton.setOnClickListener{clickMeButton.text = "Button clicked!"}
+
+
+
     }
 }
